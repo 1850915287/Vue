31c47e0f645cec7d45bc40f9353d0799e1da61eb
1850915287 <1850915287@qq.com>
2020-06-03 15:34:17 +0800

    '音乐播放'

--- 16、综合应用-音乐播放器/js/main.js
+++ 16、综合应用-音乐播放器/js/main.js
@@ -1,20 +1,38 @@
 // document.domain = "autumnfish.cn";
+// 设置axios的基地址
+// axios.defaults.baseURL = 'https://autumnfish.cn';
 var app = new Vue({
 	el: "#player",
 	data: {
+		// 搜索关键字
 		query: "",
-		musicList: []
+		// 歌曲数组
+		musicList: [],
+		// 音乐地址
+		musicUrl: ""
 	},
 	methods: {
+		// 搜索歌曲
 		searchMusic: function() {
 			var that = this
 			axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
 				function(response) {
-					console.log(response.data.result.songs[0].name);
-					that.musicList=response.data.result.songs;
+					// console.log(response);
+					that.musicList = response.data.result.songs;
 				},
 				function(Error) {
 
+				}
+			)
+		},
+		// 播放歌曲
+		playMusic: function(musicId) {
+			var that = this;
+			console.log(musicId);
+			axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
+				function(response) {
+					// console.log(response);
+					 that.musicUrl = response.data.data[0].url;
 				}
 			)
 		}
